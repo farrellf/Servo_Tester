35f836686502b9238d951e110379f5a929d6dfb9
Farrell Farahbod <upgrdman@gmail.com>
2014-09-28 19:15:25 -0700

    Added peak measurements to the display

--- screen_specan_live.c
+++ screen_specan_live.c
@@ -1,5 +1,5 @@
 // Written by: Farrell Farahbod
-// Last revised: 2014-09-15
+// Last revised: 2014-09-28
 // License: public domain
 
 #include "f0lib/f0lib_lcd_tft1p4705.h"
@@ -59,39 +59,75 @@ void drawSpecAnLiveScreen() {
 	cc2500_write_register(FSCAL0,	0x19);
 	cc2500_write_register(PATABLE,	0xFF);
 	
-	lcd_pixel_stream_start();
-
 	// draw live view
 	while(currentScreen == SPEC_AN_LIVE) {
 		#define CHANNEL_COUNT 240
-		#define HISTOGRAM_HEIGHT 240
+		#define HISTOGRAM_HEIGHT 216
 		int16_t y;
+		uint8_t history[CHANNEL_COUNT] = {0}; // store RSSI for every channel
 
 		// iterate over all channels and draw the histogram
+		lcd_pixel_stream_start();
 		for(uint8_t channel = 0; channel < CHANNEL_COUNT; channel++) {
 			cc2500_send_strobe(SIDLE);
 			cc2500_set_channel(channel);
 			cc2500_enter_rx_mode();
-			int16_t rssi_avg = cc2500_get_rssi();
+			int16_t rssi = cc2500_get_rssi();
+			history[channel] = (uint8_t) rssi;
+			
 			for(volatile uint32_t i = 0; i < 3000; i++); // not sure why this is required
 			
 			lcd_pixel_stream_move_cursor(channel*2, 40);
 			for(y = HISTOGRAM_HEIGHT - 1; y >= 0; y--) {
-				if(y <= rssi_avg)
+				if(y <= rssi)
 					lcd_pixel_stream_write_pixel(0b1111100000000000);
 				else
 					lcd_pixel_stream_write_pixel(0);
 			}
 			lcd_pixel_stream_move_cursor(channel*2+1, 40);
 			for(y = HISTOGRAM_HEIGHT - 1; y >= 0; y--) {
-				if(y <= rssi_avg)
+				if(y <= rssi)
 					lcd_pixel_stream_write_pixel(0b1111100000000000);
 				else
 					lcd_pixel_stream_write_pixel(0);
 			}
 		}
+		lcd_pixel_stream_stop();
+		
+		// calc max RSSI and display info
+		uint8_t maxRssi = 0;
+		uint8_t maxChNum = 0;
+		for(uint8_t i = 0; i < CHANNEL_COUNT; i++)
+			if(history[i] > maxRssi) {
+				maxRssi = history[i];
+				maxChNum = i;
+			}
+
+		int32_t dBm = ((int32_t) maxRssi) - 108; // convert back to raw rssi reading
+		dBm *= 5; // convert to units of 10 = 1dB
+		dBm -= 700; // remove 70dB of offset
+		dBm *= -1; // convert from negative to postiive number
+		uint32_t freq = 24000 + ((uint32_t) maxChNum) * 3.49121094f; // units of 10 = 1MHz. channel spacing = 349.121094 kHz
+
+		lcd_pixel_stream_start();
+		lcd_pixel_stream_move_cursor(maxChNum*2, 40);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_move_cursor(maxChNum*2+1, 40);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_write_pixel(0b0000011111100000);
+		lcd_pixel_stream_stop();
+
+		lcd_printf(0, 8, "  PEAK: %p41 MHz -%p21 dBm  ", freq, (uint32_t) dBm);		
 	}
-	lcd_pixel_stream_stop();
 }
 
 void notifySpecAnLiveScreen(enum Event event) {
